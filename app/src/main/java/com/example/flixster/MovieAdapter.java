83e278c01b1cecaae2ccb134cadc368db2b887fc
mahmoudagag <agagmahmoud@gmail.com>
2021-09-12 02:19:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieAdapter.java
@@ -0,0 +1,88 @@
+package com.example.flixster;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies){
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie,parent,false);
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        Movie movie =movies.get(position);
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            //String placeHolder ="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop";
+            //String placeHolder = "ic_launcher_background.xml";
+            String imageURL;
+            int height,width;
+            if(context.getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE || movie.getRating()>=8){
+                imageURL=movie.getBackdropPath();
+                width = 1400;
+                height = 1000;
+            }else {
+                imageURL = movie.getPosterPath();
+                height = 1000;
+                width = 480;
+            }
+            Glide.with(context).load(imageURL)
+                    .placeholder(R.drawable.ic_launcher_background)
+                    //.centerCrop()
+                    .override(height,width)
+                    //.dontAnimate()
+                    .into(ivPoster);
+        }
+    }
+}
